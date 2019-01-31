980f9cc138ac8f261515144756302125277b32a8
xiny <xiny@rose-hulman.edu>
2019-01-31 10:29:57 -0500

    done

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -3,47 +3,72 @@ This project lets you try out Tkinter/Ttk and practice it!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Yu Xin.
+"""  # DO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
+
+def print_hello_or_goodbye(entry_box):
+    if entry_box.get()=="ok":
+        print("hello")
+    else:
+        print("goodbye")
+
+def print_entry_for_n_times(entry_box1,entry_box2):
+    s=entry_box2.get()
+    n=int(s)
+    for k in range(n):
+        print(entry_box1.get())
 
 
 def main():
     """ Constructs a GUI with stuff on it. """
     # -------------------------------------------------------------------------
-    # TODO: 2. After reading and understanding the m1e module,
+    # DO: 2. After reading and understanding the m1e module,
     #   ** make a window that shows up. **
     # -------------------------------------------------------------------------
+    root=tkinter.Tk()
+
 
     # -------------------------------------------------------------------------
-    # TODO: 3. After reading and understanding the m2e module,
+    # DO: 3. After reading and understanding the m2e module,
     #   ** put a Frame on the window. **
     # -------------------------------------------------------------------------
+    main_frame=ttk.Frame(root,padding=50)
+    main_frame.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 4. After reading and understanding the m2e module,
+    # DO: 4. After reading and understanding the m2e module,
     #   ** put a Button on the Frame. **
     # -------------------------------------------------------------------------
+    button1=ttk.Button(root,text="hello, hello?")
+    button1.grid()
 
     # -------------------------------------------------------------------------
-    # TODO: 5. After reading and understanding the m3e module,
+    # DO: 5. After reading and understanding the m3e module,
     #   ** make your Button respond to a button-press **
     #   ** by printing   "Hello"  on the Console.     **
     # -------------------------------------------------------------------------
+    button1["command"]=(lambda:print("hello"))
 
     # -------------------------------------------------------------------------
-    # TODO: 6. After reading and understanding the m4e module,
+    # DO: 6. After reading and understanding the m4e module,
     #   -- Put an Entry box on the Frame.
     #   -- Put a second Button on the Frame.
     #   -- Make this new Button, when pressed, print "Hello"
     #        on the Console if the current string in the Entry box
     #        is the string 'ok', but print "Goodbye" otherwise.
     # -------------------------------------------------------------------------
+    a_entry_box=ttk.Entry(main_frame)
+    a_entry_box.grid()
+
+    button2=ttk.Button(root,text="print hello or goodbye")
+    button2.grid()
+    button2["command"]=(lambda:print_hello_or_goodbye(a_entry_box))
 
     # -------------------------------------------------------------------------
-    # TODO: 7.
+    # DO: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
@@ -57,6 +82,14 @@ def main():
     #    that is a "user error" -- do NOT deal with that.
     #
     # -------------------------------------------------------------------------
+    second_entry_box=ttk.Entry(main_frame)
+    second_entry_box.grid()
+
+    button3=ttk.Button(main_frame,text="print n times")
+    button3.grid()
+    button3["command"]=lambda:print_entry_for_n_times(a_entry_box,second_entry_box)
+
+    root.mainloop()
     ####################################################################
     # HINT:
     #   You will need to obtain the INTEGER from the STRING
@@ -67,7 +100,7 @@ def main():
     ####################################################################
 
     # -------------------------------------------------------------------------
-    # TODO: 8. As time permits, do other interesting GUI things!
+    # DO: 8. As time permits, do other interesting GUI things!
     # -------------------------------------------------------------------------
 
 
